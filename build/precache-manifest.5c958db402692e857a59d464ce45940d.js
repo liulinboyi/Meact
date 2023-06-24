68e11c0405b9456d8b58b7daf0f2cd51e873e4f4
liulinboyi <814921718@qq.com>
2023-06-24 19:55:17 +0800

    feat: update build

---
+++ build/precache-manifest.5c958db402692e857a59d464ce45940d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd2320b245b6e178311cba0512c109d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc9c436e8178f31bb84b",
+    "url": "./static/css/main.11cdb8ee.chunk.css"
+  },
+  {
+    "revision": "cc9c436e8178f31bb84b",
+    "url": "./static/js/main.74cecdd8.chunk.js"
+  },
+  {
+    "revision": "e1503afe6e9dd3eb9eb2",
+    "url": "./static/js/runtime-main.f3767d8b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5420f257f9779027528caf7998101b88.js"
+  "./precache-manifest.5c958db402692e857a59d464ce45940d.js"
 );
 
 self.addEventListener('message', (event) => {
